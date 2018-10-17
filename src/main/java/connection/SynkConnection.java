d13c07c68c11a2eb318ed9e22792a26377e2f985
BLeHew <bricelehew@gmail.com>
2018-10-16 22:07:21 -0500

    working with some maven settings

---
+++ src/main/java/connection/SynkConnection.java
@@ -0,0 +1,80 @@
+package connection;
+
+import java.sql.*;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class SynkConnection {
+    public static String url = "jdbc:mysql://localhost:3306/synk?allowPublicKeyRetrieval=true&useSSL=false";
+    public static String driver = "com.mysql.jdbc.Driver";
+    private static String userName = "root";
+    private static String password = "root";
+    public static Connection con;
+    public static String lastError = "";
+    public static boolean hasConnection = false;
+
+    public static void establish(){
+        try {
+            Class.forName(driver);
+        }catch(Exception e){
+            System.err.println("Error in loading driver " + e);
+        }
+        try {
+            con = DriverManager.getConnection(url, userName, password);
+            System.out.println("Successful Connection");
+            hasConnection = true;
+        } catch (SQLException e){
+            System.err.println("Error in DB connection");
+        }
+    }
+    public static boolean hasConnection(){
+        try {
+            return con.isValid(500);
+        }catch (SQLException s){
+            System.err.println("Error in checking if connection is valid");
+            System.err.println(s);
+            return false;
+        }
+    }
+    public static boolean validateCredentials(String userName,String password){
+        ResultSet rs;
+        try {
+            PreparedStatement stmt = con.prepareStatement("SELECT USERNAME,PASS_HASH FROM users WHERE USERNAME = ?");
+            stmt.setString(1,userName);
+            rs = stmt.executeQuery();
+            if(!rs.first()){
+                lastError = "Username does not exist";
+                return false;
+            }
+            if(password.hashCode() != rs.getInt("PASS_HASH")){
+                lastError = "Password Incorrect";
+                return false;
+            }
+        }catch(SQLException e){
+            lastError = "Error in SQL Connection";
+            System.err.println(e);
+            return false;
+        }
+        return true;
+    }
+    public static boolean registerCredentials(String userName, String password,String email){
+        String query = "SELECT USERNAME FROM users WHERE USERNAME = '" + userName + "'";
+        try {
+            ResultSet rs = con.createStatement().executeQuery(query);
+            if(!rs.first()){
+                query = "INSERT INTO users VALUES (null," + userName + "," + email + "," + password.hashCode() + ",1)";
+                con.createStatement().executeUpdate(query);
+                return true;
+            }else {
+                return false;
+            }
+        }catch (SQLException e){
+            System.err.println("Exception in registering credentials.");
+            System.err.println(e);
+            System.out.println(query);
+
+            return false;
+        }
+    }
+}
---
+++ src/main/java/userinterface/MainAppUI.java
@@ -0,0 +1,107 @@
+package userinterface;
+
+import TableObjects.Task;
+import TableObjects.User;
+import connection.SynkConnection;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import TableObjects.Project;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class MainAppUI {
+    ObservableList<Project> projItems = FXCollections.observableArrayList();
+    ObservableList<Task> taskItems = FXCollections.observableArrayList();
+    ObservableList<User> userItems = FXCollections.observableArrayList();
+
+    @FXML private ListView<Project> listViewProjects;
+    @FXML private ListView<Task> listViewTasks;
+    @FXML private ListView<User> listViewUsers;
+
+    //TODO move some functionality to a separate class, maybe a UI controller of some sorts
+    //TODO add functionality to only display projects that the user is on, maybe through saved session user_id
+
+    public void initialize(){
+        ResultSet rs;
+        try {
+            PreparedStatement stmt = SynkConnection.con.prepareStatement("SELECT * FROM projects");
+            rs = stmt.executeQuery();
+            while(rs.next()){
+                projItems.add(new Project(rs.getInt("PROJ_ID"),
+                        rs.getString("PROJ_NAME"),
+                        rs.getString("PROJ_DESC")));
+            }
+        } catch (SQLException e){
+            System.err.println(e);
+        }
+        listViewProjects.setItems(projItems);
+    }
+    @FXML
+    public void narrowUsers(){
+        if(listViewTasks.getSelectionModel().getSelectedIndex() ==-1){
+            return;
+        }
+        int taskID = listViewTasks.getItems().get(listViewTasks.getSelectionModel().getSelectedIndex()).getTaskID();
+
+        userItems.clear();
+        try {
+            ResultSet rs;
+            PreparedStatement stmt = SynkConnection.con.prepareStatement(
+                    "SELECT u.USER_ID,u.USERNAME\n" +
+                            " FROM users u,user_task_assigned uta\n" +
+                            " WHERE u.user_id = uta.user_id AND uta.task_id = " + taskID);
+            rs = stmt.executeQuery();
+            while(rs.next()){
+                userItems.add(new User(rs.getInt("user_id"), rs.getString("username")));
+            }
+        } catch (SQLException e){
+            System.err.println(e);
+        }
+        listViewUsers.setItems(userItems);
+    }
+    @FXML
+    public void showProjectTasksAndUsers(){
+        if(listViewProjects.getSelectionModel().getSelectedIndex() == -1){
+            return;
+        }
+        taskItems.clear();
+        userItems.clear();
+        //listViewUsers.refresh();
+        //listViewTasks.refresh();
+
+        ResultSet rs;
+        int projId = listViewProjects.getItems().get(listViewProjects.getSelectionModel().getSelectedIndex()).getProjId();
+        try {
+            PreparedStatement stmt = SynkConnection.con.prepareStatement("SELECT * FROM tasks WHERE Proj_ID =" + projId);
+            rs = stmt.executeQuery();
+            while(rs.next()){
+                taskItems.add(new Task(
+                        rs.getInt("TASK_ID"),
+                        rs.getString("TASK_DESC"),
+                        rs.getString("TASK_NAME"),
+                        rs.getInt("PROJ_ID")));
+            }
+        } catch (SQLException e){
+            System.err.println(e);
+        }
+        try {
+            PreparedStatement stmt = SynkConnection.con.prepareStatement(
+                    "SELECT * FROM users u,user_proj_assigned upa " +
+                            "WHERE u.user_id = upa.user_id AND upa.proj_id = " +  projId);
+            rs = stmt.executeQuery();
+            while(rs.next()){
+                userItems.add(new User(rs.getInt("user_id"), rs.getString("username")));
+            }
+        } catch (SQLException e){
+            System.err.println(e);
+        }
+        listViewTasks.setItems(taskItems);
+        listViewUsers.setItems(userItems);
+    }
+
+}
