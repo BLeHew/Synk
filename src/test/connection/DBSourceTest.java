2dcf44260e47cf1442288a22ff3e92e05be24d3e
BLeHew <bricelehew@gmail.com>
2018-11-20 15:31:08 -0600

    refactoring mainAppUi

---
+++ src/test/connection/DBSourceTest.java
@@ -0,0 +1,18 @@
+package connection;
+
+import org.junit.jupiter.api.Test;
+import tableobjects.Project;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DBSourceTest {
+
+    @Test
+    void runQuery() {
+    }
+
+    @Test
+    void insertAssignment() {
+
+    }
+}
